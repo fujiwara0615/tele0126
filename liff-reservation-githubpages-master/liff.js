769df347d1959f8778d436914b0e1c6b3bb89ff5
fujiwara0615 <69880914+fujiwara0615@users.noreply.github.com>
2021-01-26 16:34:01 +0900

    Add files via upload

--- liff-reservation-githubpages-master/liff.js
+++ liff-reservation-githubpages-master/liff.js
@@ -1,58 +1,6 @@
-// $(document).ready(function () {
-//     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
-//     // LINE DevelopersのLIFF画面より確認可能
-//     var liffId = "1655609572-PaOgWdOL";
-//     initializeLiff(liffId);
-// })
-
-// function initializeLiff(liffId) {
-//     liff
-//         .init({
-//             liffId: liffId
-//         })
-//         .then(() => {
-//             // Webブラウザからアクセスされた場合は、LINEにログインする
-//             if (!liff.isInClient() && !liff.isLoggedIn()) {
-//                 window.alert("LINEアカウントにログインしてください。");
-//                 liff.login({redirectUri: location.href});
-//             }
-//         })
-//         .catch((err) => {
-//             console.log('LIFF Initialization failed ', err);
-//         });
-// }
-
-// function sendText(text) {
-//     if (!liff.isInClient()) {
-//         shareTargetPicker(text);
-//     } else {
-//         sendMessages(text);
-//     }
-// }
-
-// // LINEトーク画面上でメッセージ送信
-// function sendMessages(text) {
-//     liff.sendMessages([{
-//         'type': 'text',
-//         'text': text
-//     }]).then(function () {
-//         liff.closeWindow();
-//     }).catch(function (error) {
-//         window.alert('Failed to send message ' + error);
-//     });
-// }
-
-// // Webブラウザからメッセージ送信
-// function shareTargetPicker(text) {
-//     liff.shareTargetPicker([{
-//         'type': 'text',
-//         'text': text
-//     }]).catch(function (error) {
-//         window.alert('Failed to send message ' + error);
-//     });
-// }
-
 $(document).ready(function () {
+    // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
+    // LINE DevelopersのLIFF画面より確認可能
     var liffId = "1655609572-PaOgWdOL";
     initializeLiff(liffId);
 })
@@ -63,9 +11,10 @@ function initializeLiff(liffId) {
             liffId: liffId
         })
         .then(() => {
+            // Webブラウザからアクセスされた場合は、LINEにログインする
             if (!liff.isInClient() && !liff.isLoggedIn()) {
                 window.alert("LINEアカウントにログインしてください。");
-                liff.login();
+                liff.login({redirectUri: location.href});
             }
         })
         .catch((err) => {
@@ -73,14 +22,15 @@ function initializeLiff(liffId) {
         });
 }
 
-function sendMessage(text) {
-    if (liff.isInClient()) {
-        sendMessages(text);
-    } else {
+function sendText(text) {
+    if (!liff.isInClient()) {
         shareTargetPicker(text);
+    } else {
+        sendMessages(text);
     }
 }
 
+// LINEトーク画面上でメッセージ送信
 function sendMessages(text) {
     liff.sendMessages([{
         'type': 'text',
@@ -92,6 +42,7 @@ function sendMessages(text) {
     });
 }
 
+// Webブラウザからメッセージ送信
 function shareTargetPicker(text) {
     liff.shareTargetPicker([{
         'type': 'text',
