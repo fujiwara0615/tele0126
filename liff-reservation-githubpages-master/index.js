b2d2a1f771d0b3b1b80d7b41314c562e12256603
fujiwara0615 <69880914+fujiwara0615@users.noreply.github.com>
2021-01-26 15:09:39 +0900

    Add files via upload

---
+++ liff-reservation-githubpages-master/index.js
@@ -0,0 +1,20 @@
+$(function () {
+    // 送信
+    $('form').submit(function () {
+        var sub_type = $('input[name="sub_type"]:checked').val();
+        var camp = $('input[name="camp_name"]').val();
+        var par = $('input[name="par_name"]').val();
+        var tele = $('input[name="tele"]').val();
+        var textbox = $('input[name="textbox"]').val();
+        var subtext = $('input[name="subtext"]').val();
+        // $('#form-name').children().each(function (i, elm) {
+        //     names += $(elm).val() + '、';
+        // })
+        // names = names.slice(0, -1);
+
+        var msg = `種別：${sub_type}\n会社名：${camp}\n担当者氏名：${par}\n電話番号：${tele}\n内容：${textbox}\n備考：${subtext}`;
+        sendText(msg);
+        
+        return false;
+    });
+});
