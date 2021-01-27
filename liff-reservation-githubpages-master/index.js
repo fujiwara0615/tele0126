b84587bd79e8c94e5fdd9ae5d5223845de4af6de
fujiwara0615 <69880914+fujiwara0615@users.noreply.github.com>
2021-01-27 12:52:53 +0900

    Add files via upload

--- liff-reservation-githubpages-master/index.js
+++ liff-reservation-githubpages-master/index.js
@@ -1,7 +1,8 @@
 $(function () {
     // 送信
     $('form').submit(function () {
-        var sub_type = $('input[name="sub_type"]:checked').val();
+        var sub_type = $('input[name="sub_type"]').val();
+        var from = $('input[name="from_p"]').val();
         var camp = $('input[name="camp_name"]').val();
         var par = $('input[name="par_name"]').val();
         var tele = $('input[name="tele"]').val();
@@ -13,7 +14,7 @@ $(function () {
         // })
         // names = names.slice(0, -1);
 
-        var msg = `種別：${sub_type}\n会社名：${camp}\n担当者氏名：${par}\n電話番号：${tele}\n内容：${textbox}\n備考：${subtext}`;
+        var msg = `種別：${sub_type}\n${from}\n会社名：${camp}\n担当者氏名：${par}\n電話番号：${tele}\n内容：${textbox}\n備考：${subtext}`;
         sendText(msg);
         
         return false;
